539887fd75a0fec5a3f573fbbf2618fd7a236780
Victor Pedrazzi <vspedrazzi@gmail.com>
2020-12-14 23:08:36 -0300

    feat(todos) add form for adding todos

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
-import { TasksCollection } from '/imports/api/TasksCollection';
 import { Task } from './Task';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { TaskForm } from './TaskForm';
  
 export const App = () => {
-  const tasks = useTracker(() => TasksCollection.find({}).fetch());
- 
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
   return (
     <div>
       <h1>Welcome to Meteor!</h1>
- 
+
+      <TaskForm/>
+
       <ul>
         { tasks.map(task => <Task key={ task._id } task={ task }/>) }
       </ul>
